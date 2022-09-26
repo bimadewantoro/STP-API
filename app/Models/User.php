12f37ae5ce81544636ab2ae11195976204083a33
SonyPintoko <sony.pintoko@students.utdi.ac.id>
2022-09-26 11:27:09 +0700

    User Profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-use Tymon\JWTAuth\Contracts\JWTSubject;
-use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable implements JWTSubject
 {
@@ -71,5 +72,10 @@ class User extends Authenticatable implements JWTSubject
                 'roles' => $this->getRoleNames(),
             ],
         ];
+    }
+
+    public function userProfile()
+    {
+        return $this->hasOne(UserProfile::class);
     }
 }
