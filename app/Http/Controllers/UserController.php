5a065290d853eeff0186dfcd0981269b7c893d19
Bima Dewantoro <54613336+bimadewantoro@users.noreply.github.com>
2022-10-07 14:04:11 +0700

    add doc api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,13 +28,6 @@ class UserController extends Controller
     {
         //
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     
     public function store(Request $request)
     {
