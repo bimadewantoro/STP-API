49000b1a4876e201db0065740c98e889d7bcaed5
Bima Dewantoro <54613336+bimadewantoro@users.noreply.github.com>
2022-10-09 16:10:30 +0700

    fix login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
