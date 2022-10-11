3af4baf92c5a43e3b5c7a35ae3c15488e6261f88
achmadraja <rajmandmkgmailcom>
2022-10-11 13:41:24 +0700

    password required

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Password;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password as FacadesPassword;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -12,7 +14,14 @@ class AuthController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'email' => 'required|string|email|max:255',
-            'password' => 'required|string',
+            'password' => ['required',
+            'string',
+            'min:8',             // must be at least 10 characters in length
+            'regex:/[a-z]/',      // must contain at least one lowercase letter
+            'regex:/[A-Z]/',      // must contain at least one uppercase letter
+            'regex:/[0-9]/',      // must contain at least one digit
+            'regex:/[@$!%*#?&.,+_-]/', // must contain a special character
+            ],
         ]);
 
         if ($validator->fails()) {
