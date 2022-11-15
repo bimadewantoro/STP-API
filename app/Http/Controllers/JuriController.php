ac26d8d308c092c3b0f14dcf60d84623e55ed030
bimadewantoro <bimadewantoro@students.unnes.ac.id>
2022-11-15 22:43:46 +0700

    add docs

--- app/Http/Controllers/JuriController.php
+++ app/Http/Controllers/JuriController.php
@@ -7,23 +7,12 @@ use Illuminate\Http\Request;
 
 class JuriController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $juri = User::role('juri')->get();
         return response()->json($juri);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -47,12 +36,6 @@ class JuriController extends Controller
         ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $request = User::find($id);
@@ -62,13 +45,6 @@ class JuriController extends Controller
         ], 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $request->validate([
@@ -91,12 +67,6 @@ class JuriController extends Controller
         ], 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $user = User::find($id);
