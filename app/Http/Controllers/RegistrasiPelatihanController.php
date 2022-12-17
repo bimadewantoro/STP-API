de5e34c9edd51c591808b59118204d547ee61a83
bimadewantoro <bimadewantoro@students.unnes.ac.id>
2022-12-17 15:15:29 +0700

    fix verification controller

--- app/Http/Controllers/RegistrasiPelatihanController.php
+++ app/Http/Controllers/RegistrasiPelatihanController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pelatihan;
+use App\Models\RegistrasiPelatihan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class RegistrasiPelatihanController extends Controller
 {
-    //
+    public function index()
+    {
+        $registrasiPelatihan = RegistrasiPelatihan::all();
+        return $registrasiPelatihan;
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'pelatihan_id' => 'required',
+            'profile_talent_id' => 'required',
+            'status' => 'required',
+            'bukti_pembayaran_path' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()->first(),
+            ], 400);
+        }
+
+        $registrasiPelatihan = RegistrasiPelatihan::create([
+            'pelatihan_id' => $request->pelatihan_id,
+            'profile_talent_id' => $request->profile_talent_id,
+            'status' => $request->status,
+            'bukti_pembayaran_path' => $request->bukti_pembayaran_path,
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $registrasiPelatihan,
+        ]);
+    }
 }
--- app/Models/RegistrasiPelatihan.php
+++ app/Models/RegistrasiPelatihan.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class RegistrasiPelatihan extends Model
 {
     use HasFactory;
+
+    protected $table = 'registrasi_pelatihans';
+
+    protected $fillable = [
+        'pelatihan_id',
+        'profile_talent_id',
+        'status',
+        'bukti_pembayaran_path',
+    ];
+
+    public function pelatihan()
+    {
+        return $this->belongsTo(Pelatihan::class, 'pelatihan_id', 'id');
+    }
+
+    public function profile_talent()
+    {
+        return $this->belongsTo(ProfileTalent::class, 'profile_talent_id', 'id');
+    }
 }
